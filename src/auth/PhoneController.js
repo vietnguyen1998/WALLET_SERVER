6b22024bd9848e49b725dc3358cc546570906909
Admin <Admin@DESKTOP-A0QR3FR>
2020-01-20 23:06:44 +0700

    VIET

--- src/auth/PhoneController.js
+++ src/auth/PhoneController.js
@@ -1,4 +1,4 @@
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var utils = require('../utils');
@@ -16,33 +16,58 @@ var config = require('../../config'); // get config file
 
 router.post('/paymentPhoneCart', async function (req, res, next) {
     try {
+        network = req.body.network;
+        phone = req.body.phone;
+        money = req.body.money;
+
+        let seri = '';
+        let cardNum = '';
+        for (var i = 0; i < 13; i++) {
+            seri += '' + Math.floor(Math.random() * 10) + '';
+        }
+        for (var i = 0; i < 12; i++) {
+            cardNum += '' + Math.floor(Math.random() * 10) + '';
+        }
         if (false) { // send money = false
             // response 
-            return res.status(500).send({ auth: false, error: true, message: "server error!" });
-        } 
+            return res.status(500).send({ auth: false, error: true, message: "Thanh toán thất bại" });
+        }
         // update balance
-
+        const db = req.app.get('db');
+        db.utilFuncs.updateDecreaseBalance(req.body.phone, Number.parseInt(req.body.money));
         // return TRUE
-        return res.status(200).send({ error: false });
+        return res.status(200).send({ error: false, seriNum: seri, cardNum: cardNum, message: ('Thanh toán thành công thẻ Viettel ' + req.body.money) });
     } catch (error) {
-        return res.status(500).send({ auth: false, error: true, message: "some error!" });
+        return res.status(500).send({ auth: false, error: true, message: "Thanh toán thất bại" });
     }
 });
 
 router.post('/paymentPhone', async function (req, res, next) {
     try {
+        network = req.body.network;
+        phone = req.body.phone;
+        money = req.body.money;
+
+        let seri = '';
+        let cardNum = '';
+        for (var i = 0; i < 13; i++) {
+            seri += '' + Math.floor(Math.random() * 10) + '';
+        }
+        for (var i = 0; i < 12; i++) {
+            cardNum += '' + Math.floor(Math.random() * 10) + '';
+        }
         if (false) { // send money = false
             // response 
-            return res.status(500).send({ auth: false, error: true, message: "server error!" });
+            return res.status(500).send({ auth: false, error: true, message: "Thanh toán thất bại" });
         }
+        // send a messager to phone
         // update balance
-
-        // send message to phone
-
+        const db = req.app.get('db');
+        db.utilFuncs.updateDecreaseBalance(req.body.phone, Number.parseInt(req.body.money));
         // return TRUE
-        return res.status(200).send({ error: false });
+        return res.status(200).send({ error: false, seriNum: seri, cardNum: cardNum, message: ('Thanh toán thành công thẻ Viettel ' + req.body.money) });
     } catch (error) {
-        return res.status(500).send({ auth: false, error: true, message: "some error!" });
+        return res.status(500).send({ auth: false, error: true, message: "Thanh toán thất bại" });
     }
 });
 
