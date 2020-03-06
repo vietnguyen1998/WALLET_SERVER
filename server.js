3fb558db11e61b3e086153243f2a272c36c2c335
16130 <16130@DESKTOP-B3LS0MK>
2020-03-06 22:56:55 +0700

    v3

--- server.js
+++ server.js
@@ -20,18 +20,23 @@ const fs = require('fs');
 const formidableMiddleware = require('express-formidable');
 app.use(formidableMiddleware({ uploadDir: './public' }));
 app.post('/upload', async (req, res) => {
+    console.log("begin update image...")
     fs.rename(req.files.avatar.path, req.files.avatar.path + '.jpg', err => {
-        res.send('Upload Failed')
+        res.send('Upload Failed');
     });
+    console.log("rename image...");
     try {
         const db = req.app.get('db');
         var image = '/' + req.files.avatar.path + '.jpg';
         try {
-            await db.usersettings.UpdateImg(image, req.files.avatar.name);
+            console.log("return image...");
+            console.log(image);
+            console.log(req.fields.info);
+            await db.usersettings.UpdateImg("/public\upload_32d5b61e748b10711073071923001c1d.jpg", req.fields.info);
         } catch (e) {
             //return res.status(500).send({ auth: false, error: true, errmessage: "some error2!" });
         }
-        //return res.status(200).send({ auth: true, error: false });
+        return res.status(200).send({ auth: true, error: false });
     } catch (error) {
         //return res.status(500).send({ auth: false, error: true, errmessage: "error" });
     }
