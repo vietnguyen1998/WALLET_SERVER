d2f778db756c2441812fda033fbc02b9cc068d89
16130 <16130@DESKTOP-B3LS0MK>
2020-03-15 17:35:47 +0700

    Viet. 1

--- server.js
+++ server.js
@@ -20,24 +20,19 @@ const fs = require('fs');
 const formidableMiddleware = require('express-formidable');
 app.use(formidableMiddleware({ uploadDir: './public' }));
 app.post('/upload', async (req, res) => {
-    console.log("begin update image...")
+    console.log("begin update image...", req.files.avatar.path)
     fs.rename(req.files.avatar.path, req.files.avatar.path + '.jpg', err => {
-        res.send('Upload Failed');
-    });
-    console.log("rename image...");
-    try {
-        const db = req.app.get('db');
-        var image = '/' + req.files.avatar.path + '.jpg';
-        try {
-            await db.usersettings.UpdateImg(image, req.fields.info);
-        res.status(200).send({ auth: true, error: false, image: image });
-        } catch (e) {
-            //   return res.status(500).send({ auth: false, error: true, errmessage: "some error2!" });
+        if (err == null) {
+            try {
+                var image = '/' + req.files.avatar.path + '.jpg';
+                const db = req.app.get('db');
+                db.usersettings.UpdateImg(image, req.fields.info);
+                return res.status(200).send({ auth: true, error: false, imgUrl: image });
+            } catch (error) {
+                return res.status(500).send({ auth: false, error: true });
+            }
         }
-        console.log("return image...")
-    } catch (error) {
-        // return res.status(500).send({ auth: false, error: true, errmessage: "error" });
-    }
+    });
 });
 
 // static folder
